6cc23a3e3f7f9c03f1456ff0c9b987cf37f2864c
Edu <edu.centurion.funes@gmail.com>
2019-09-01 20:21:27 -0400

    CRUD Categorias

--- src/app/pages/categoria/categoria.component.ts
+++ src/app/pages/categoria/categoria.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoriaService } from 'src/app/services/categoria.service';
+
+declare const $: any;
 
 @Component({
   selector: 'app-categoria',
@@ -7,9 +10,76 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriaComponent implements OnInit {
 
-  constructor() { }
+  private data : any[] = [];
+  private count : Number = 0;
+  private nueva_categoria: String = null;
+  private edit_categoria: any = {
+    descripcion : null
+  };
+  private delete_categoria : any ={
+    descripcion : null
+  };
+
+  constructor(private service:CategoriaService) { }
 
   ngOnInit() {
+    this.getData()
+    /*/let b = true;
+    setInterval(()=>{
+      $("#addModal").modal('show')
+    },3000)*/
   }
 
+  getData(){
+    this.service.all().subscribe((response)=>{
+      this.data = response['lista']
+      this.count = response['totalDatos']
+    })
+  }
+
+  closeAdd(send){
+    if(send){
+      this.service.post({
+        'descripcion' : this.nueva_categoria
+      }).subscribe(()=>{
+        this.getData();
+      })
+    }
+    this.nueva_categoria = null
+    $("#addModal").modal('hide');
+  }
+
+  openEdit(to_edit){
+    this.edit_categoria = JSON.parse(JSON.stringify(to_edit))
+    $("#editModal").modal('show');
+  }
+
+  closeEdit(send){
+    if(send){
+      this.service.put(this.edit_categoria).subscribe(()=>{
+        this.getData();
+      })
+    }
+    this.edit_categoria = {
+      descripcion : null
+    }
+    $("#editModal").modal('hide');
+  }
+
+  openDelete(to_delete){
+    this.delete_categoria = JSON.parse(JSON.stringify(to_delete))
+    $("#deleteModal").modal('show');
+  }
+
+  closeDelete(send){
+    if(send){
+      this.service.delete(this.delete_categoria['idCategoria']).subscribe(()=>{
+        this.getData();
+      })
+    }
+    this.delete_categoria = {
+      descripcion : null
+    }
+    $("#deleteModal").modal('hide');
+  }
 }
--- src/app/services/categoria.service.ts
+++ src/app/services/categoria.service.ts
@@ -1,10 +1,31 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CategoriaService {
-  private url_base = "Asd";
+  private url_base = "https://gy7228.myfoscam.org:8443/stock-pwfe/";
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public all(){
+    let url = this.url_base + "categoria"
+    return this.http.get(url)
+  }
+
+  public post(data){
+    let url = this.url_base + "categoria"
+    return this.http.post(url,data)
+  }
+
+  public put(data){
+    let url = this.url_base + "categoria"
+    return this.http.put(url,data)
+  }
+
+  public delete(id){
+    let url = this.url_base + "categoria/" + id
+    return this.http.delete(url)
+  }
 }
