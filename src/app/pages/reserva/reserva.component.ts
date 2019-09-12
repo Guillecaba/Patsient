7e7b602f2a73a141b370a41d68f4a27f9dbdc45a
augusto95 <augsilhu@hotmail.com>
2019-09-11 23:39:42 -0400

    listado reserva v1

--- src/app/pages/reserva/reserva.component.ts
+++ src/app/pages/reserva/reserva.component.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ReservaService } from 'src/app/services/reserva.service';
 
 @Component({
   selector: 'app-reserva',
   templateUrl: './reserva.component.html',
   styleUrls: ['./reserva.component.css']
 })
+
 export class ReservaComponent implements OnInit {
 
-  constructor() { }
+  fecha_inicio: Date;
+  fecha_fin: Date;
+  idPaciente: number;
+  idProfesional: number;
+  reservas: any;
+
+  constructor(public reservaService: ReservaService) { }
 
   ngOnInit() {
+    console.log('aloooo');
+    this.reservaService.getTodasLasReservas().subscribe((res: any) => (
+      this.reservas = res['lista']
+    ));
   }
-
 }
