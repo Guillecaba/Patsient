b77ea97b8fa3a29228a2da1946554fd3a545d550
Ramiro <ramirox95@hotmail.com>
2019-09-12 13:15:14 -0400

    lista reservas v2

--- src/app/pages/reserva/reserva.component.ts
+++ src/app/pages/reserva/reserva.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ReservaService } from 'src/app/services/reserva.service';
+import { PacienteService } from 'src/app/services/paciente.service';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-reserva',
@@ -9,17 +11,65 @@ import { ReservaService } from 'src/app/services/reserva.service';
 
 export class ReservaComponent implements OnInit {
 
-  fecha_inicio: Date;
-  fecha_fin: Date;
-  idPaciente: number;
-  idProfesional: number;
-  reservas: any;
+  // inputs
+  fecha1;
+  fecha2;
 
-  constructor(public reservaService: ReservaService) { }
+  reservas: any;
+  pacientes: any;
+  empleados: any;
+  fecha_inicio: string;
+  fecha_fin: string;
+  actualCliente: number;
+  actualClienteNombre: string;
+  actualEmpleado: number;
+  actualEmpleadoNombre: string;
+  constructor(public reservaService: ReservaService, public pacienteService: PacienteService, public datePipe: DatePipe) { }
 
   ngOnInit() {
-    console.log('aloooo');
+    this.pacienteService.getTodos().subscribe((res: any) => (
+      this.pacientes = res['lista']
+    ));
+    this.pacienteService.getTodosEmpleados().subscribe((res: any) => (
+      this.empleados = res['lista']
+    ));
     this.reservaService.getTodasLasReservas().subscribe((res: any) => (
+      this.reservas = res['lista']
+    ));
+  }
+
+  setCliente(cliente: any) {
+    this.actualCliente = cliente['idPersona'];
+    this.actualClienteNombre = cliente['nombre'];
+    console.log('Cliente: ' + this.actualCliente + '\nEmpleado: ' + this.actualEmpleado);
+  }
+  setEmpleado(empleado: any) {
+    this.actualEmpleado = empleado['idPersona'];
+    this.actualEmpleadoNombre = empleado['nombre'];
+    console.log('Cliente: ' + this.actualCliente + '\nEmpleado: ' + this.actualEmpleado);
+  }
+  setFechaInicio(event: any) {
+    this.fecha_inicio = event.target.value;
+    this.fecha_inicio = this.datePipe.transform(this.fecha_inicio, 'yyyyMMdd');
+    console.log('Fecha ini:' + this.fecha_inicio + '\nFecha fin:' + this.fecha_fin);
+  }
+  setFechaFin(event: any) {
+    this.fecha_fin = event.target.value;
+    this.fecha_fin = this.datePipe.transform(this.fecha_fin, 'yyyyMMdd');
+    console.log('Fecha ini:' + this.fecha_inicio + '\nFecha fin:' + this.fecha_fin);
+  }
+  limpiarFiltros() {
+    this.fecha_inicio = null;
+    this.fecha_fin = null;
+    this.actualEmpleado = null;
+    this.actualEmpleadoNombre = null;
+    this.actualCliente = null;
+    this.actualClienteNombre = null;
+    this.fecha1 = null;
+    this.fecha2 = null;
+  }
+  buscar() {
+    this.reservaService.getReservas(this.fecha_inicio, this.fecha_fin, this.actualCliente, this.actualEmpleado).subscribe((res: any) => (
       this.reservas = res['lista']
     ));
   }
