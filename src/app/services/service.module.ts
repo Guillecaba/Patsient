d6810433ae174a6fac07b706987e8dc387f47f35
augusto95 <augsilhu@hotmail.com>
2019-09-11 23:39:31 -0400

    servicio de reserva importado

--- src/app/services/service.module.ts
+++ src/app/services/service.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PacienteService } from './paciente.service';
+import { ReservaService } from './reserva.service';
 
 @NgModule({
   declarations: [],
@@ -8,7 +9,8 @@ import { PacienteService } from './paciente.service';
     CommonModule
   ],
   providers: [
-    PacienteService
+    PacienteService,
+    ReservaService
   ]
 })
 export class ServiceModule { }
--- src/app/services/services.module.ts
+++ src/app/services/services.module.ts
@@ -1,14 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CategoriaService } from './categoria.service';
+import { ReservaService } from './reserva.service';
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule
   ],
-  providers:[
-    CategoriaService
+  providers: [
+    CategoriaService,
+    ReservaService
   ]
 })
 export class ServicesModule { }
