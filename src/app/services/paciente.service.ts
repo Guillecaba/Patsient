791522e9e99804c2440f85df1ae6ae6e4e6ae6d2
augusto95 <augsilhu@hotmail.com>
2019-09-13 22:33:43 -0400

    correciones menores paciente service

--- src/app/services/paciente.service.ts
+++ src/app/services/paciente.service.ts
@@ -9,24 +9,24 @@ import { JsonPipe } from '@angular/common';
 export class PacienteService {
   private url_base = "https://gy7228.myfoscam.org:8443/stock-pwfe/";
   //URL ='https://gy7228.myfoscam.org:8443/stock-pwfe/persona';
- 
-  constructor(public  http: HttpClient) { 
-    
 
-   }
-   public all(){
+  constructor(public http: HttpClient) {
+
+
+  }
+  public all() {
     let url = this.url_base + "persona"
     return this.http.get(url)
   }
 
-  public get(filters){
+  public get(filters) {
     let separator = '?'
     let url = this.url_base + "persona"
-    for(let k in filters){
-      if(filters[k] == null) {
+    for (let k in filters) {
+      if (filters[k] == null) {
         continue
       }
-      url= url + separator + k + "=" +filters[k]
+      url = url + separator + k + "=" + filters[k]
       separator = "&"
     }
     return this.http.get(url)
@@ -43,45 +43,45 @@ export class PacienteService {
   }
 
 
-  public post(data){
+  public post(data) {
     const body = JSON.stringify(data);
     let url = this.url_base + "persona"
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-     
+
     });
 
-    return this.http.post(url,data, { headers })
+    return this.http.post(url, data, { headers })
   }
 
-  public put(data){
+  public put(data) {
     const body = JSON.stringify(data);
     let url = this.url_base + "persona/"
     const headers = new HttpHeaders({
       'Content-Type': 'application/json',
-     
+
     });
 
-    return this.http.put(url,data,{ headers })
+    return this.http.put(url, data, { headers })
   }
 
-  public delete(id){
+  public delete(id) {
     let url = this.url_base + "persona/" + id
     return this.http.delete(url)
   }
-
-
-  public getTodos() {
-    let url = this.url_base + 'persona?orderBy=apellido&orderDir=asc';
-    return this.http.get(url);
-  }
-
-  public getTodosEmpleados() {
-    let url = this.url_base + 'persona?orderBy=apellido&orderDir=asc&ejemplo=%7B"soloUsuariosDelSistema"%3Atrue%7D';
-    return this.http.get(url);
-  }
-
-
- 
+  /*
   
+    public getTodos() {
+      let url = this.url_base + 'persona?orderBy=apellido&orderDir=asc';
+      return this.http.get(url);
+    }
+  
+    public getTodosEmpleados() {
+      let url = this.url_base + 'persona?orderBy=apellido&orderDir=asc&ejemplo=%7B"soloUsuariosDelSistema"%3Atrue%7D';
+      return this.http.get(url);
+    }
+  */
+
+
+
 }
